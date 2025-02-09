fc28db2575ebb5d7180f4a6eb33593db114045b9
tertemiss <126486116+tertemiss@users.noreply.github.com>
2025-02-09 12:43:20 +0300

    Create monitor.sh

---
+++ monitor.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+FILE="/root/output.txt"
+LOG="/root/access.log"
+DURATION=120
+> "$LOG"
+
+inotifywait -m -e modify "$FILE" --format '%T %w%f' --timefmt '%s' | while read -r timestamp file; do
+    lsof "$FILE" | awk '{print $2}' | grep -v PID >> "$LOG" 
+done &
+
+sleep $DURATION
+pkill -P  $$ inotifywait
+
+PID=$(sort "$LOG" | uniq -c | sort -nr | head -n 1 | awk '{print $2}')
+
+echo "$PID" > /root/answer.txt
